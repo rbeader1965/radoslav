88208dfbb4ae8bcc149832acefc055a74328cec6
RAY <raybeader@hotmail.com>
2023-02-15 16:27:26 -0800

    commit

---
+++ .vercel/output/functions/fn-0.func/Users/radoslavbeader/radoslav/.svelte-kit/vercel-tmp/manifest.js
@@ -0,0 +1,27 @@
+export const manifest = {
+	appDir: "_app",
+	appPath: "_app",
+	assets: new Set(["favicon.png"]),
+	mimeTypes: {".png":"image/png"},
+	_: {
+		entry: {"file":"_app/immutable/start-66cf787b.js","imports":["_app/immutable/start-66cf787b.js","_app/immutable/chunks/index-65bc829e.js","_app/immutable/chunks/singletons-efdfcb03.js"],"stylesheets":[],"fonts":[]},
+		nodes: [
+			() => import('../output/server/nodes/0.js'),
+			() => import('../output/server/nodes/1.js'),
+			() => import('../output/server/nodes/2.js')
+		],
+		routes: [
+			{
+				id: "/",
+				pattern: /^\/$/,
+				params: [],
+				page: { layouts: [0], errors: [1], leaf: 2 },
+				endpoint: null
+			}
+		],
+		matchers: async () => {
+			
+			return {  };
+		}
+	}
+};
---
+++ .vercel/output/static/_app/immutable/chunks/2-2e291c10.js
@@ -0,0 +1,1 @@
+import{default as t}from"../components/pages/_page.svelte-5ec5fb14.js";export{t as component};
